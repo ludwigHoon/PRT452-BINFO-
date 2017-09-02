df6d1b8727ce17a50b4b2b6756ef9472354528cf
ludwigdH <30543314+ludwigdH@users.noreply.github.com>
2017-09-02 13:53:38 +0930

    Changing project structure

---
+++ minSNP/R/flag.R
@@ -0,0 +1,37 @@
+#' \code{usualLength} is used to find out 
+#' the length of the sequence (W/O deletion).
+#' @param seq a list of SeqFastadna. To keep it simple, 
+#' use read.fasta from seqinr to import the fasta file.
+#' @examples 
+#' library(seqinr)
+#' chlamydia <- read.fasta('Chlamydia.fas')
+#' usualLength(chlamydia)
+#' @return Will return the maximum length of all the allelic profiles.
+usualLength<-function(seq){
+	max=length(seq[[1]])
+	for(i in 2:length(seq)){
+		if(max<length(seq[[i]])){
+			max=length(seq[[i]])
+		}
+	}
+return(max)
+}
+
+#' \code{flagAllele} is used to find out a list of allelic profiles
+#' that has been flagged and will not be included in computation
+#' of minimum SNPs. 
+#' @inheritParams usualLength
+#' @examples
+#' flagAllele(Chlamydia)
+#' @return Will return a list of ignored allelic profiles.
+flagAllele<-function(seq){
+	ignoreList=vector('character')
+	normal=usualLength(seq)
+	for(i in names(seq)){
+		if(length(seq[[i]])<normal){
+			ignoreList=c(ignoreList, i)
+		}
+	}
+return(ignoreList)
+}
+
--- test/unit2.R
+++ test/unit2.R
@@ -5,22 +5,19 @@
 # 3. Chlamydia_2.fas   --- (expecting 5 allele flagged - at position 1, 3, 56)
 	
 
-test.flag <- function()
+test.similar <- function()
 {
-	#When the sequence within the test file are all of the same length
+	#Returning a list of SNPs (100) with percentage
 	a <<- read.fasta(file='../resource/Chlamydia_mapped_Frankenstein_SNPs_Ns_to_Gs_oneofeachaussie.fas')
 	checkIdentical(flagAllele(a), vector('character'))
 	
-	#When there is a sequence within the test file that is not of the same length
-	a <<- read.fasta(file='../resource/Chlamydia_1.fas')
+	#Returning a list of SNPs (100) with percentage (Also have to take into account of included loci)
+	includedLoci <- c(3,4,5)
 	checkIdentical(flagAllele(a), c('A_D213'))
 	
-	#When there is multiple sequences within test file that are not of the same length
-	a <<- read.fasta(file='../resource/Chlamydia_2.fas')
-	checkIdentical(flagAllele(a), c('A_D213', 'Ia_SotoGIa3', 'D_SotoGD1'))	
 }
 
 test.deactivation <- function()
 {
-  DEACTIVATED('Deactivating flag test function')
+  DEACTIVATED('Deactivating similar test function')
 }
