38aa7380717c030bfbac4dd81edfc3df0b81598f
ludwigdH <30543314+ludwigdH@users.noreply.github.com>
2017-09-15 10:15:35 +0930

    Refactored

--- minSNP/R/dMode.R
+++ minSNP/R/dMode.R
@@ -7,76 +7,55 @@
 #' @return Will return the a list of SNPs that can be used 
 #' and the associated percentage at the particular location.
 library(rlist)
-similar.simpson <-function(seq, level=1, resFromUp=NULL){
+
+similar.simpson <-function(seq, level=1, included=NULL, excluded=NULL){
 	N=length(seq)
 	result=list()
-	if ((level==1)&&is.null(resFromUp)){
-		curRes=list()
-		for(position in 1:length(seq[[1]])){
-		type=list()
+	lvl=1
+	type=list()
+	appended=list()
+	explored=vector('numeric')
+	if(!is.null(included)){
+		for (inc in included){
+			type=simpson.pattern(seq, inc, appended)
+			simpIndex=simpson.calculate(type, N)
 			for(al in 1:length(seq)){
-				if (is.null(type[[seq[[al]][position]]])){
-					type[[seq[[al]][position]]]=1}
-				else{
-					type[[seq[[al]][position]]]=type[[seq[[al]][position]]]+1
-				}		
-		}
-		tsum=0
-		for(t in type){
-			tsum=tsum+(t*(t-1))
-		}
-		simpIndex=1-(tsum/(N*(N-1)))
-		curRes[[position]]=list(value=simpIndex)
-	}
-	position=list.order(curRes, (value))[1]
-	index=as.numeric(curRes[[list.order(curRes, (value))[1]]]['value'])
-	
-	result[[1]]=list(position=position, index=index)
-	}
-	
-	else{
-		if (length(resFromUp) < (level-1)){
-			result=similar.simpson(seq, (level-1), resFromUp)
-			resFromUp=result
-		}
-		
-		explored=vector('numeric')
-		appended=list()
-		for (a in 1:length(resFromUp)){
-			explored=c(explored, resFromUp[[a]]$'position')
-			po=resFromUp[[a]]$'position'
-			for (al in 1:length(seq)){
-				appended[[getName(seq[[al]])]]=paste(appended[[getName(seq[[al]])]], seq[[al]][po], sep="")
+			appended[[getName(seq[[al]])]]=paste(appended[[getName(seq[[al]])]], seq[[al]][inc], sep="")
 			}
+			explored=c(explored, inc)
+			result[[lvl]]=list(position=inc,index=simpIndex)
+			lvl=lvl+1
 		}
-		#print(appended)
-		curRes=list()
-		for(position in 1:length(seq[[1]])){
-		type=list()
+	}
+	for(a in 1:level){
+	curRes=list()
+	explored=c(explored, excluded)
+	for(position in 1:length(seq[[1]])){
 		if (position %in% explored){next}
-			for(al in 1:length(seq)){
-				name= paste(appended[[getName(seq[[al]])]], seq[[al]][position], sep="")
-				if (is.null(type[[name]])){
-					type[[name]]=1}
-				else{
-					type[[name]]=type[[name]]+1
-				}
-		}
-		tsum=0
-		for(t in type){
-			tsum=tsum+(t*(t-1))
-		}
-		simpIndex=1-(tsum/(N*(N-1)))
+		type=simpson.pattern(seq, position, appended)
+		simpIndex=simpson.calculate(type, N)
 		curRes[[position]]=list(position=position, value=simpIndex)	
-		}
+		}	
 		explored=sort(explored, decreasing=TRUE)
 		for (a in explored){ curRes[[a]]<-NULL}
 		position=list.order(curRes, (value))[1]
 		index=as.numeric(curRes[[position]]['value'])
 		position=curRes[[position]]$position
-		result[[level]]=list(position=position,index=index)
+		result[[lvl]]=list(position=position,index=index)
+		lvl=lvl+1
+		for(al in 1:length(seq)){
+			appended[[getName(seq[[al]])]]=paste(appended[[getName(seq[[al]])]], seq[[al]][position], sep="")
+			}
+		explored=c(explored, position)
 	}
+	# excl=''
+	# incl=''
+	# for(exc in excluded){excl=paste(excl, exc)}
+	# for(inc in included){incl=paste(incl, inc)}
+	# result['excluded']=excl
+	# result['included']=incl
 	return(result)
+	
 }
 
 present.simpson <-function(seq, result){
@@ -94,8 +73,33 @@ present.simpson <-function(seq, result){
 	for (a in 1:length(appended)){
 		type[[appended[[a]]]]=c(type[[appended[[a]]]], names(appended)[a])
 	}
-	
-	
+	#inc=paste('included:', result['included'])
+	#exc=paste('excluded:', result['excluded'])
+	#descrip=paste(descrip, inc, exc, sep='\n')
 	type[['Description']]=descrip
 	return(type)
 }
+
+simpson.calculate<-function(pattern, N){
+	tsum=0
+	for(t in pattern){
+		tsum=tsum+(t*(t-1))
+	}
+	simpIndex=1-(tsum/(N*(N-1)))
+	return(simpIndex)
+}
+
+simpson.pattern<-function(seq, position, appended=NULL)
+{
+	type=list()
+			for(al in 1:length(seq)){
+				name= paste(appended[[getName(seq[[al]])]], seq[[al]][position], sep="")
+				if (is.null(type[[name]])){
+					type[[name]]=1}
+				else{
+					type[[name]]=type[[name]]+1
+				}
+		}
+	return(type)
+
+}				
