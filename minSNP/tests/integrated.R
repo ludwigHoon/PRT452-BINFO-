bb61ef73d95a4741695447e802e528b31d20f22a
ludwigdH <30543314+ludwigdH@users.noreply.github.com>
2017-10-05 11:03:44 +0930

    travis-ci test

--- minSNP/tests/integrated.R
+++ minSNP/tests/integrated.R
@@ -36,12 +36,45 @@ sample.case1 <-function(){
 	checkEquals(output[[3]]$'Index', 0.7266, tolerance=0.00016)
 	Description=paste('At position:', '2034', sep='-')
 	checkEquals(output[[3]]$'Description', Description)
-	
-	
 }
 
 sample.case2 <-function(){
-
+	#Read the file
+	Chlamydia <- read.fasta(file='../data/Chlamydia_1.txt')
+	
+	#STEP 1. Process the file
+	Chlamydia <- processAllele(Chlamydia)
+	
+	#Since Chlamydia is normal
+	checkEquals(length(Chlamydia), )
+	
+	#PERCENT MODE
+	result=similar.percent(Chlamydia, 'A_D213')
+	present=present.percent(result, 98, 100)
+	
+	#All result should have percent higher or equal to 98
+	for (a in 1:100){
+	checkTrue(present[[a]]$percent>=98)
+	}
+	
+	#SIMPSON MODE
+	result=branch.simpson(Chlamydia, level=1, numRes=3)
+	output=present.simpson(Chlamydia, result)
+	
+	#Should have 3 results
+	checkTrue(length(output)==3)
+	
+	checkEquals(output[[1]]$'Index',  0.7344, tolerance=0.00016)
+	Description=paste('At position:', '1988', sep='-')
+	checkEquals(output[[1]]$'Description', Description)
+	
+	checkEquals(output[[2]]$'Index', 0.7318, tolerance=0.00016)
+	Description=paste('At position:', '2044', sep='-')
+	checkEquals(output[[2]]$'Description', Description)
+	
+	checkEquals(output[[3]]$'Index', 0.7266, tolerance=0.00016)
+	Description=paste('At position:', '2034', sep='-')
+	checkEquals(output[[3]]$'Description', Description)
 }
 
 test.deactivation <- function()
--- minSNP/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(minSNP)
-
-test_check("minSNP")
